4398f0e5bfcf037f264a79b8893a24dc0778a8de
Paris Karagiannopoulos <parisk85@gmail.com>
2020-02-16 00:32:48 +0200

    Login form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import RegisterForm from './auth/RegisterForm'
+import LoginForm from './auth/LoginForm'
 
 function App() {
   return (
-    <div className="App">
-      <RegisterForm />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={LoginForm} />
+          <Route path='/confirm/*' component={LoginForm} />
+          <Route exact path='/auth/login' component={LoginForm} />
+          <Route exact path='/auth/register' component={RegisterForm} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/auth/LoginForm.js
+++ src/auth/LoginForm.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react'
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-class RegisterForm extends Component {
+class LoginForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
             username: "",
             password: "",
-            passwordConfirm: "",
-            email: "",
-            submitted: false
+            redirect: false,
+            validation: ""
         };
     }
 
@@ -20,29 +19,41 @@ class RegisterForm extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        const authRequest = {
+        const loginRequest = {
             'username': this.state.username,
             'password': this.state.password,
-            'passwordConfirm': this.state.passwordConfirm,
-            'email': this.state.email
         }
-        axios.post('http://localhost:8080/auth/register', authRequest)
+        axios.post('http://localhost:8080/auth/authenticate', loginRequest)
             .then((response) => {
-                if (response.status === 201) {
-                    this.setState({ submitted: true });
+                if (response.status === 200) {
+                    this.setState({ redirect: true })
                 }
             }).catch((error) => {
                 console.log(error.response);
             });
     }
 
+    handleConfirmation = () => {
+        if (this.state.validation === "") {
+            const confirmationToken = this.props.location.pathname.split('/').slice(-1);
+            console.log(confirmationToken);
+            axios.patch(`http://localhost:8080/auth/confirm/${confirmationToken}`)
+                .then((response) => {
+                    console.log(response);
+                    this.setState({ validation: 'success' });
+                }).catch((error) => {
+                    this.setState({ validation: 'error' });
+                });
+        }
+    }
+
     render() {
         return (
             <div className="container">
                 <div className="row row-padding">
-                    {(!this.state.submitted) ?
+                    {!this.props.location.pathname.startsWith('/confirm') ?
                         <div className="col-md-4 offset-md-4">
-                            <h4>register</h4>
+                            <h4>login</h4>
                             <form onSubmit={this.handleSubmit}>
                                 <div className="form-group">
                                     <label className="d-flex p-2" htmlFor="username">username</label>
@@ -52,24 +63,21 @@ class RegisterForm extends Component {
                                     <label className="d-flex p-2" htmlFor="password">password</label>
                                     <input type="password" className="form-control" name="password" value={this.state.password} onChange={this.handleChange} placeholder="password" />
                                 </div>
-                                <div className="form-group">
-                                    <label className="d-flex p-2" htmlFor="confirm-password">confirm password</label>
-                                    <input type="password" className="form-control" name="passwordConfirm" value={this.state.passwordConfirm} onChange={this.handleChange} placeholder="password" />
-                                </div>
-                                <div className="form-group">
-                                    <label className="d-flex p-2" htmlFor="email">e-mail</label>
-                                    <input type="email" className="form-control" name="email" value={this.state.email} onChange={this.handleChange} placeholder="e-mail" />
-                                </div>
                                 <button type="submit" className="btn btn-primary">Submit</button>
-                                <small id="login-redirect" className="form-text text-muted">Click here to <Link to='/auth/login'>login</Link></small>
+                                <small id="register-redirect" className="form-text text-muted">Click here to <Link to='/auth/register'>register</Link></small>
                             </form>
                         </div> :
                         <div className="col-md-12">
+                            {this.handleConfirmation()}
                             <div className="card">
                                 <div className="card-body">
-                                    <p className="lead"><small>
-                                        You have successfully submitted your information. An email will be send to <mark>{this.state.email}</mark>. Please follow its instructions to complete your registration.
-                                </small></p>
+                                    <p className="lead">
+                                        {(this.state.validation === 'success' ?
+                                            <small>Your account has been activated. Click <Link to='/auth/login'>here</Link> to log in.</small> :
+                                            <small>An error occured while trying to activate your account. Click <Link to='/auth/register'>here</Link> to register.</small>
+                                        )}
+                                        {this.state.message}
+                                    </p>
                                 </div>
                             </div>
                         </div>
@@ -80,4 +88,4 @@ class RegisterForm extends Component {
     }
 }
 
-export default RegisterForm;
+export default LoginForm;
